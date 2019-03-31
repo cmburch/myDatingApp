c43ced717735e912cbbee75c8efd1511ee408fea
Charles Burch <georgia_beast89@yahoo.com>
2019-03-31 09:13:03 -0500

    updated the login now using AuthO library

--- DatingApp-Spa/src/app/_services/auth.service.ts
+++ DatingApp-Spa/src/app/_services/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 
   constructor(private http: HttpClient) {}
 
@@ -22,5 +24,9 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-Spa/src/app/nav/nav.component.ts
+++ DatingApp-Spa/src/app/nav/nav.component.ts
@@ -22,8 +22,9 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
