f1b787c9fdc30f37a5b2ffc4fa4bd79ba986bbc6
Charles Burch <georgia_beast89@yahoo.com>
2019-03-30 21:37:45 -0500

    passed the values from home to register component

--- DatingApp-Spa/src/app/home/home.component.ts
+++ DatingApp-Spa/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode:boolean = false;
-  constructor() { }
+  values:any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
+
+     getValues() {
+     this.http.get('http://localhost:5000/api/values/')
+     .subscribe(res => {
+       this.values = res;
+     }, err => {
+       // this will be logging service in the future
+       console.log(err);
+     });
+   }
 }
