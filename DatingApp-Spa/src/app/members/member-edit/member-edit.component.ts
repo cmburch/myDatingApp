f4ac5482c4f5d5558f773b7ac0446a069cc4e5c1
Charles Burch <georgia_beast89@yahoo.com>
2019-04-25 14:47:27 -0500

    created member edit template with update user method need to implement the user update API

--- DatingApp-Spa/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-Spa/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../_models/user';
+import { UserService } from '../../_services/user.service';
+import { AuthService } from '../../_services/auth.service';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,14 +12,30 @@ import { User } from '../../_models/user';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
   }
+
+  updateUser() {
+    // this is just a test to see object being update after save button
+    console.log(this.user);
+    this.editForm.reset(this.user);
+  }
+
+  // updateUser() {
+  //   this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+  //     this.editForm.reset(this.user);
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  // }
 
 }
