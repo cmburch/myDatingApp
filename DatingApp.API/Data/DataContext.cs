c1f0a326a86488b16bee6f299ee9c3f542e2e39d
Charles Burch <georgia_beast89@yahoo.com>
2019-04-28 09:45:57 -0500

    created the Like Entity for the user in the API and generated to Likes table

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,6 +1,13 @@
+using System;
+using DatingApp.API.Data;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
 namespace DatingApp.API.Data
 {
     public class DataContext : DbContext 
@@ -9,6 +16,25 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values {get; set; } 
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // creates the primary key
+            builder.Entity<Like>()
+                .HasKey(k => new {k.LikerId, k.LikeeId});
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+             builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
