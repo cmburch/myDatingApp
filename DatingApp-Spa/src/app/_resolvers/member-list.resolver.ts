5d6998719922f86278e75a2dcd64c04bbf245ff5
Charles Burch <georgia_beast89@yahoo.com>
2019-04-27 21:08:30 -0500

    setup up pagination in Angular ,updated the getUsers method http headers

--- DatingApp-Spa/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-Spa/src/app/_resolvers/member-list.resolver.ts
@@ -7,10 +7,13 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
-    constructor(private userService: UserService, private router: Router) {}
+  pageNumber = 1;
+  pageSize = 5;
+
+  constructor(private userService: UserService, private router: Router) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.router.navigate(['/home']);
                 return of(null);
