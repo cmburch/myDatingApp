00e6a5022ecd9d1befbc12511d153dd6a89af3fe
Charles Burch <georgia_beast89@yahoo.com>
2019-04-27 21:29:26 -0500

    added ngx-bootstrap and updated the pagination methods in angular

--- DatingApp-Spa/src/app/members/member-list/member-list.component.ts
+++ DatingApp-Spa/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/user';
 import { UserService } from './../../_services/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from '../../_models/pagination';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-list',
@@ -10,22 +12,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
-  constructor(private userService: UserService, private route: ActivatedRoute) { }
+  constructor(private userService: UserService, private route: ActivatedRoute,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
     // this.loadUser();
       this.route.data.subscribe(data => {
       this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
   }
-  //  loadUser() {
-  //   this.userService.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  //     console.log(this.users );
 
-  //   }, error => {
-  //     console.log(error);
-  //   });
-  //  }
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe((res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+    }, error => {
+      this.toastr.error(error);
+    });
+  }
 }
