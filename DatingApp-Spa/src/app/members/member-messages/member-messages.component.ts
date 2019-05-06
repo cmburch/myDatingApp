9d0ff49e0e3d5cd6d0a966513514b7ea31b217c8
Charles Burch <georgia_beast89@yahoo.com>
2019-05-06 09:13:53 -0500

    added the mark as read functionality to the API and angular app

--- DatingApp-Spa/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-Spa/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from '../../_models/message';
 import { UserService } from '../../_services/user.service';
 import { AuthService } from '../../_services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -22,14 +23,33 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId )
+    const currentUserId = +this.authService.decodedToken.nameid;
+    this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
       .subscribe(messages => {
         this.messages = messages;
-      }, error => {
-        this.toastr.error(error);
-      });
-
+    }, error => {
+      this.toastr.error(error);
+    });
   }
+
+  // loadMessages() {
+  //   this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId )
+  //     .subscribe(messages => {
+  //       this.messages = messages;
+  //     }, error => {
+  //       this.toastr.error(error);
+  //     });
+
+  // }
 
   sendMessage() {
     this.newMessage.recipientId = this.recipientId;
