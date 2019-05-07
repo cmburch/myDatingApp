e358e341610dfc8ae5f3b7df328fc3535ef3f199
Charles Burch <georgia_beast89@yahoo.com>
2019-05-07 18:23:20 -0500

    setup the angular app to run from kestrel server

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -98,7 +98,15 @@ namespace DatingApp.API
             // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                name: "spa-fallback",
+                defaults: new { controller = "Fallback", action = "Index"}
+                );
+             });
+            // app.UseMvc();
         }
     }
 }
